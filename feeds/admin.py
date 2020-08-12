3c1a885b1091d3b99f6674f2d882fd4518500fd5
sonnguyen2506 <sonnguyen.tech@gmail.com>
2020-08-12 11:18:11 +0700

    Add setup_db command

--- feeds/admin.py
+++ feeds/admin.py
@@ -10,7 +10,8 @@ class FeedAdmin(admin.ModelAdmin):
     list_display = ['title', 'link', 'guid', 'pubDate']
     list_filter = ['Category_id']
     search_fields = ['title']
+    list_per_page = 15
 
 
-#admin.site.register(Category)
+admin.site.register(Category)
 admin.site.register(Feed, FeedAdmin)
